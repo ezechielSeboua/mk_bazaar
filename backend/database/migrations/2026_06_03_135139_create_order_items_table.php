ffe93836096c0d6edb1abb00051cfcd33e1a4473
ezechielSeboua <ezechiel.seboua@epitech.eu>
2026-06-15 21:16:35 +0000

    Fix: modification de la migration pour permettre la suppression de produits avec variantes

--- backend/database/migrations/2026_06_03_135139_create_order_items_table.php
+++ backend/database/migrations/2026_06_03_135139_create_order_items_table.php
@@ -13,16 +13,20 @@ return new class extends Migration
     {
         Schema::create('order_items', function (Blueprint $table) {
             $table->id();
-            
+
             // Clé étrangère vers la commande parente (on supprime les lignes si la commande est supprimée)
             $table->foreignId('order_id')->constrained()->onDelete('cascade');
-            
+
             // Clé étrangère vers la variante du produit (on empêche la suppression de la variante si elle est dans une commande)
-            $table->foreignId('product_variant_id')->constrained('product_variants')->onDelete('restrict');
-            
+            // $table->foreignId('product_variant_id')->constrained('product_variants')->onDelete('restrict'); // POur eviter de supprimer un produit tant qu'il à des variants
+            $table->foreignId('product_variant_id')
+                ->nullable()
+                ->constrained('product_variants')
+                ->nullOnDelete();
+
             $table->integer('quantity'); // Quantité achetée
             $table->integer('price'); // Prix unitaire en centimes figé au moment de l'achat
-            
+
             $table->timestamps();
         });
     }
